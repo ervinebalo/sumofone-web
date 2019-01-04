4d51a2876182ed205df9da3ce80d7573ddce8a8c
Ervin Ebalo <eebalo@gmail.com>
2019-01-03 21:47:52 -0800

    footer ui update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,32 +10,38 @@ const Footer = class extends React.Component {
  render() {
    return (
     <footer className="footer has-background-dark">
-      <div className="content has-text-centered has-text-light">
-        <p>Follow Us</p>
-        <p>
-          <a
-            href="https://instagram.com/sumofoneorg"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{padding: '10px', color: 'white'}}
-          >
-            <span className="icon">
-              <i className="fab fa-instagram fa-2x"></i>
-            </span>
-          </a>
-          
-          <a
-            href="https://facebook.com/sumofoneorg"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{padding: '10px', color: 'white'}}
-          >
-            <span className="icon">
-              <i className="fab fa-facebook fa-2x"></i>
-            </span>
-          </a>
-        </p>
-        <p>Copyright 2019</p>
+      <div className="container">
+        <div class="columns ">
+          <div class="column has-text-light">
+            <p>Copyright 2019</p>
+          </div>
+          <div class="column has-text-right">
+            {/* <p>Connect With Us</p> */}
+            <p>
+              <a
+                href="https://instagram.com/sumofoneorg"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{padding: '10px', color: 'white'}}
+              >
+                <span className="icon">
+                  <i className="fab fa-instagram fa-2x"></i>
+                </span>
+              </a>
+              
+              <a
+                href="https://facebook.com/sumofoneorg"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{padding: '10px', color: 'white'}}
+              >
+                <span className="icon">
+                  <i className="fab fa-facebook fa-2x"></i>
+                </span>
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   

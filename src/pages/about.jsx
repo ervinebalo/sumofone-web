0efad4507ba71a7fb6e2b856938c06d6f1cd1c72
Ervin Ebalo <eebalo@gmail.com>
2019-01-05 22:24:45 -0800

    all images on left

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -25,10 +25,14 @@ export default () => (
   {/* <PageHeaderQuote quote="BELIEVE WITH US THAT THE FUTURE CAN BE BETTER THAN TODAY"
                     author="" /> */}
   
-
+  {/* About Us */}
   <div className="section ">
     <div className="container">
       <div className="columns is-multiline   ">
+        <div className="column is-half">
+          <img src={lauren_and_ervin_image} alt="Lauren and Ervin Ebalo" />
+        </div>
+
         <div className="column is-half">
           <h3 className="title is-3 has-text-grey-darker ">WHO WE ARE</h3>
           <div className={containerStyles.separator}></div>
@@ -40,13 +44,11 @@ export default () => (
           We are people that believe we can do something to make a difference for someone. We both grew up in very family oriented homes that emphasized and exemplified what it meant to put other people’s needs before our own. So it only felt natural when we got married that we would carry on that legacy and do something to give back as well. Although the details of how and where we grew up were vastly different, the basic elements of who we are and the value we hold melded together in a pretty cool way, inspiring us to form the Sum of One. Our hope is that as we include our kids in these outreaches and fundraising, they will grow up to value and experience the same kind of life perspective.
           </p>
         </div>
-        <div className="column is-half">
-          <img src={lauren_and_ervin_image} alt="Lauren and Ervin Ebalo" />
-        </div>
       </div>
     </div>
   </div>
 
+  {/* Lauren */}
   <div className="section has-background-white-ter">
     <div className="container">
       <div className="columns is-multiline   ">
@@ -65,10 +67,15 @@ export default () => (
     </div>
   </div>
 
-
+  {/* Ervin */}
   <div className="section ">
     <div className="container">
       <div className="columns is-multiline ">
+
+        <div className="column is-half">
+          <img src={ervin_image} alt="Ervin Ebalo" />
+        </div>
+
         <div className="column is-half">
           <h3 className="title is-3 has-text-grey-darker is-uppercase">Ervin</h3>
           <div className={containerStyles.separator}></div>
@@ -76,10 +83,6 @@ export default () => (
           <p>
           I was born in Olongapo City on the island of Luzon in the Philippines and moved with my family to the city of Norwalk, CA when I was 8. I have very few memories of the Philippines and didn’t really get a true appreciation of my culture until I was much older. Generosity, service, and genuine love for others is what my parents have both consistently displayed and instilled in me and my siblings. This put a desire in my heart to give back but I didn’t figure out how to execute it until I met my wife. This came to life in the form of The Sum of One.
           </p>
-        </div>
-
-        <div className="column is-half">
-          <img src={ervin_image} alt="Ervin Ebalo" />
         </div>
       </div>
     </div>

e32380ec91d901a7c487a6adbfd7d58806ca9dd9
Ervin Ebalo <eebalo@gmail.com>
2019-01-06 00:03:41 -0800

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,19 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section class="hero is-fullheight-with-navbar is-primary">
+        <div class="hero-body">
+          <div class="container">
+          <h1 class="title is-1">Uh oh!</h1>
+            <p class="title">
+              The page you're looking for can't be found.
+            </p>
+            <p class="">
+              Try again or <a href="/contact" className="button">send us a message</a> to let us know what you're looking for.
+            </p>
+          </div>
+        </div>
+      </section>
   </Layout>
 )
 
